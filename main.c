8a7472bed00859f836769ad406c1781d20afa574
abc <abc@gmail.com>
2025-07-25 08:34:39 +0000

    added correct main.c file

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
+#include "functions.h"
 
-main() {
-	biggest3();
-	factorial();
-	biggest2();
-	pallindrome();
-	sumof2();
-	fibanaci();
-	sortingno();
+int main() {
+    biggest3();
+    factorial();
+    biggest2();
+    pallindrome();
+    sumof2();
+    fibanaci();
+    sortingno();
+    return 0;
 }
